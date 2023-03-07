bffe82e06c6ad4aa30f1e3a4b547633dff2b3a8e
unknown <katakuna2011@hotmail.com>
2023-03-07 18:20:27 -0300

    Curso finalizado

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,60 +1,34 @@
 ﻿using ByteBankIO;
 using System.Text;
 
-class Program
+partial class Program
 {
     static void Main(string[] args)
     {
-        var enderecoDoArquivo = "contas.txt"; //Onde o arquivos está
+        //Console.WriteLine("Digite seu nome:");
+       // var nome = Console.ReadLine();
 
-        using(var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open)) //Fluxo de dados Recebe endereço e operação(abrir)
+        var linhas = File.ReadAllLines("contas.txt");
+        Console.WriteLine(linhas.Length);
+        /*
+        foreach ( var linha in linhas)
         {
-            var numeroDeByteslidos = -1;
-                 
-
-            /*public override int Read(byte[] array, int offset, int count);
-                public override int Read -> lê o arquivo, recebe três argumentos(byte[] array -> onde os bytes lidos serão armazenados (conhecido como buffer),
-                                                                                    int offset -> delimita o indice em que o método irá começar a preencher o array,
-                                                                                        int count -> quantas posições serão preenchidas);
-            */
-
-            var buffer = new byte[1024]; // 1KB. Armazena em um array os pedaços do arquivo
-
-            while(numeroDeByteslidos != 0)
-            {
-                numeroDeByteslidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-                Console.WriteLine($"Bytes lidos: {numeroDeByteslidos}");
-                EscreverBuffer(buffer, numeroDeByteslidos);
-            }
-
-       
-            /*fluxoDoArquivo.Read(buffer, 0, 1024); ->public override int Read(byte[] array, int offset, int count);
-                Lê da posição 0 até a 1024 em imprime os bytes até o final do arquivo e então passa a retornar zeros
-            */
-
-            fluxoDoArquivo.Close();
-
-            Console.ReadLine();
-        }        
-    }
-
-    static void EscreverBuffer(byte[] buffer, int bytesLidos) //Método que escreve na tela os bytes achados no arquivo, recebe o buffer (byte[] array) como parâmetro e é chamado no main
-    {
-        var utf8 = new UTF8Encoding();
-
-        var texto = utf8.GetString(buffer, 0, bytesLidos);
-
-        //public virtual string GetString(byte[] bytes, int index, int count);
-
-        Console.Write(texto);
-
-;
-
-        /*foreach (var meuByte in buffer)
-        {
-            Console.Write(meuByte);
-            Console.Write(" ");
+            Console.WriteLine(linha);
         }
         */
+
+        var byteArquivo = File.ReadAllBytes("contas.txt");
+        Console.WriteLine($"Arquivo contas.txt possui {byteArquivo.Length} bytes");
+
+        File.WriteAllText("escrevendoComAClasseFile.txt", "Testando File.WriteAllText");
+
+        Console.WriteLine("Aplicação Finalizada...");
+
+        Console.ReadLine();
+        
+
     }
+
+
+
 }
